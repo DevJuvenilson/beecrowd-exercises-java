95e444d1955d99ef66f9d6ad36a72ee3352a1b40
Juvenilson <daniel.ribeiroemails@gmail.com>
2026-04-09 19:23:39 -0300

    First commit

---
+++ src/exercise1001.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class exercise1001 {
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int x = a + b;
+
+        System.out.println("X = " + x);
+
+        sc.close();
+
+    }
+
+}
---
+++ src/exercise1021.java
@@ -0,0 +1,34 @@
+import java.io.IOException;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class exercise1021 {
+    public static void main(String[] args) throws IOException {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        float n = sc.nextFloat();
+
+        long totalCents = Math.round(n * 100);
+
+        int[] notes = {10000, 5000, 2000, 1000, 500, 200};
+        int[] coins = {100, 50, 25, 10, 5, 1};
+
+        System.out.println("NOTAS:");
+        for (int note : notes) {
+            long count = totalCents / note;
+            System.out.printf("%d nota(s) de R$ %.2f\n", count, note / 100.0);
+            totalCents %= note;
+        }
+
+        System.out.println("MOEDAS:");
+        for (int coin : coins) {
+            long count = totalCents / coin;
+            System.out.printf("%d moeda(s) de R$ %.2f\n", count, coin / 100.0);
+            totalCents %= coin;
+        }
+
+        sc.close();
+
+    }
+}
